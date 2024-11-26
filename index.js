09958c71578debbc20da3d1ae73b1d02f56fe29e
zcorw <zhzhwwt@gmail.com>
2024-11-26 10:55:01 +0800

    完成功能开发

--- index.js
+++ index.js
@@ -11,16 +11,116 @@ function setDataNoSelect(element) {
   }
 }
 
-export default class TextSelectTrigger {
-  constructor(el) {
-    if (!(el instanceof Element || typeof el === 'string')) {
-      throw new TypeError('el must be an Element or a selector');
+function addNoSelectAttribute(element) {
+  const allDescendants = element.querySelectorAll("*");
+  allDescendants.forEach(setDataNoSelect);
+}
+
+function getText(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    return node.textContent;
+  }
+  if (node.nodeType === Node.ELEMENT_NODE) {
+    if (node.hasAttribute("data-noSelect")) {
+      return "";
     }
-    if (typeof el === 'string') {
+    return Array.from(node.childNodes).map(getText).join("");
+  }
+}
+
+export default class TextSelectTrigger {
+  constructor(el, callback) {
+    if (!(el instanceof Element || typeof el === "string")) {
+      throw new TypeError("el must be an Element or a selector");
+    }
+    if (typeof el === "string") {
       el = document.querySelector(el);
     }
-    const allDescendants = el.querySelectorAll("*");
-    allDescendants.forEach(setDataNoSelect);
+    if (typeof callback !== "function") {
+      throw new TypeError("callback must be a function");
+    }
+
+    const observer = new MutationObserver(() => {
+      addNoSelectAttribute(el);
+    });
+
+    observer.observe(el, {
+      childList: true,
+      subtree: true,
+    });
+
+    addNoSelectAttribute(el);
     this.el = el;
+    this.timeStamp = 0;
+    this.waitUp = false;
+    this.observe = observer;
+    this.callback = callback;
+    this.handler = this._handler.bind(this);
+    this.removeListener = this._removeEventListener.bind(this);
+    this.selectText = this._selectText.bind(this);
+  }
+
+  _selectText(ev) {
+    const nowStamp = Date.now();
+    this.removeListener();
+    if (nowStamp - this.timeStamp > 200) {
+      setTimeout(() => {
+        const sel = window.getSelection();
+        if (sel.rangeCount > 0 && sel.toString().length > 0) {
+          const range = sel.getRangeAt(0);
+          if (
+            !(
+              range.commonAncestorContainer === this.el ||
+              this.el.contains(range.commonAncestorContainer)
+            )
+          ) {
+            throw new Error("不支持跨文档选择");
+          }
+          const fragment = range.cloneContents();
+          let selectedText = "";
+          if (fragment.childNodes.length > 0) {
+            // 遍历文档片段，提取其中可选的文本
+            fragment.childNodes.forEach((node) => {
+              const text = getText(node);
+              if (text) {
+                selectedText += text;
+              }
+            });
+          } else {
+            selectedText = sel.toString();
+          }
+          if (selectedText.trim() !== "")
+            this.callback({
+              text: selectedText,
+              x: ev.clientX,
+              y: ev.clientY,
+            });
+        }
+      });
+    }
+    this.timeStamp = 0;
+  }
+
+  _removeEventListener() {
+    this.waitUp = false;
+    document.removeEventListener("mouseup", this.selectText);
+    document.removeEventListener("dragend", this.removeListener);
+  }
+
+  _handler() {
+    if (!this.waitUp) {
+      this.waitUp = true;
+      this.timeStamp = Date.now();
+      document.addEventListener("mouseup", this.selectText);
+      document.addEventListener("dragend", this.removeListener);
+    }
+  }
+
+  bind() {
+    this.el.addEventListener("mousedown", this.handler);
+  }
+
+  unbind() {
+    this.el.removeEventListener("mousedown", this.handler);
   }
 }
